45815b4171532b3b73cbdf077251e61038b19099
bornav <borna.vincek1@gmail.com>
2025-07-11 16:16:53 +0200

    Add way to specify externalConfig using configPath option
    DELETE-ME

--- nixos/modules/services/networking/headscale.nix
+++ nixos/modules/services/networking/headscale.nix
@@ -33,6 +33,20 @@ in
     services.headscale = {
       enable = lib.mkEnableOption "headscale, Open Source coordination server for Tailscale";
 
+      configPath = lib.mkOption {
+        type = lib.types.nullOr lib.types.path;
+        description = ''
+          Load configuration from FILE.
+
+          ::: {.note}
+          These values must be defined as follows:
+            disable_check_updates: true
+            unix_socket: /run/headscale/headscale.sock
+          :::
+        '';
+        default = null;
+      };
+
       package = lib.mkPackageOption pkgs "headscale" { };
 
       user = lib.mkOption {
@@ -581,7 +595,8 @@ in
   config = lib.mkIf cfg.enable {
     assertions = [
       {
-        assertion = with cfg.settings; dns.magic_dns -> dns.base_domain != "";
+        # assertion = with cfg.settings; dns.magic_dns -> dns.base_domain != "";
+        assertion = cfg.configPath != null || (with cfg.settings; dns.magic_dns -> dns.base_domain != "");
         message = "dns.base_domain must be set when using MagicDNS";
       }
       (assertRemovedOption [ "settings" "acl_policy_path" ] "Use `policy.path` instead.")
@@ -646,7 +661,7 @@ in
           export HEADSCALE_DATABASE_POSTGRES_PASS="$(head -n1 ${lib.escapeShellArg cfg.settings.database.postgres.password_file})"
         ''}
 
-        exec ${lib.getExe cfg.package} serve --config ${configFile}
+        exec ${lib.getExe cfg.package} serve --config ${if cfg.configPath != null then cfg.configPath else configFile}
       '';
 
       serviceConfig =
